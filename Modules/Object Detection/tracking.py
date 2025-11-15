146a3624cb6fe33ac0e53ac8629974bfe5a7806a
Armando2030 <armandogs2030@gmail.com>
2025-11-14 20:59:23 -0600

    tracking

--- Modules/Object Detection/tracking.py
+++ Modules/Object Detection/tracking.py
@@ -3,17 +3,18 @@ from ultralytics import YOLO
 from collections import defaultdict
 import numpy as np
 
-
 model = YOLO('Modules/Object Detection/best.pt').to('mps')
 cap = cv2.VideoCapture(0)
 track_history = defaultdict(lambda: [])
+colors = [(255,0,0), (0,255,0), (0,0,255), (255,255,0), (255,0,255), 
+          (0,255,255), (128,0,128), (255,128,0)] 
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-    
     results = model.track(frame, persist=True, device='mps', verbose=False)
+    
     if results[0].boxes.id is not None:
         boxes = results[0].boxes.xywh.cpu()
         track_ids = results[0].boxes.id.int().cpu().tolist()
@@ -24,14 +25,13 @@ while cap.isOpened():
             track.append((float(x), float(y)))
             if len(track) > 30:
                 track.pop(0)
-            
+    
+            color = colors[track_id % len(colors)]
             points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
             cv2.polylines(frame, [points], isClosed=False, 
-                         color=(230, 0, 0), thickness=2)
-    
+                         color=color, thickness=2)
     annotated_frame = results[0].plot()
     cv2.imshow('Tracking', annotated_frame)
-    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
